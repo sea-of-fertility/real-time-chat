6cbc85f9c089b13f8d72c933174188ae2862b4a3
hj.park <hjpark3@okestro.com>
2026-03-18 18:32:34 +0900

    Initial commit

---
+++ src/test/java/com/study/realtimechat/RealTimeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.realtimechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
