5eb17188727c05680e258c48a853670d236c49f5
hj.park <hjpark3@okestro.com>
2026-03-19 14:18:32 +0900

    feat: user repository 생성

---
+++ src/main/java/com/study/realtimechat/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.study.realtimechat.repository;
+
+import com.study.realtimechat.model.entity.User;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+    Mono<User> findByEmail(Mono<String> email);
+}
